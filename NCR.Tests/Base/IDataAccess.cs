40f73fb198a643bed8eaa33dab961eda3c023d6a
yanghongjie <yanghongjie@xlobo.com>
2019-04-23 18:21:57 +0800

    新增自定义规则运算

---
+++ NCR.Tests/Base/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace NCR.Tests.Base
+{
+    public interface IDataAccess
+    {
+        int ExecuteNonQuery(string commandText, object objParams = null);
+    }
+}
--- NCR/Internal/RuleComputeBase.cs
+++ NCR/Internal/RuleComputeBase.cs
@@ -9,76 +9,9 @@ namespace NCR.Internal
 {
     public class RuleComputeBase : RuleCompute
     {
-        protected override bool ComputeInternal(RuleItem ruleItem, Fact fact)
+        protected override bool CustomCompute(string computeType, string ruleValue, string factValue)
         {
-            #region 参数校验
-            if (ruleItem.IsNull())
-            {
-                throw new ArgumentNullException(nameof(ruleItem));
-            }
-            if (fact.IsNull())
-            {
-                throw new ArgumentNullException(nameof(fact));
-            }
-            if (ruleItem.ComputeType.IsNullOrEmpty())
-            {
-                throw new ArgumentNullException(nameof(ruleItem.ComputeType));
-            }
-            if (ruleItem.Value.IsNullOrEmpty())
-            {
-                throw new ArgumentNullException(nameof(ruleItem.Value));
-            }
-            #endregion
-
-            var ruleValue = ruleItem.Value.Trim().ToLower();
-            var computeType = (BaseComputeType)Enum.Parse(typeof(BaseComputeType), ruleItem.ComputeType);
-
-            foreach (var factKey in fact.Keys)
-            {
-                var factName = factKey.Trim().ToLower();
-                var factValue = fact[factName].Trim().ToLower();
-
-                if (factKey != ruleItem.RuleItemType) continue;
-
-                return BaseCompute(computeType, ruleValue, factValue);
-            }
-
-            return false;
-        }
-
-        protected bool BaseCompute(BaseComputeType computeType,string ruleValue,string factValue)
-        {
-            switch (computeType)
-            {
-                case BaseComputeType.LessThan:
-                    return Convert.ToDecimal(factValue) < Convert.ToDecimal(ruleValue);
-                case BaseComputeType.MoreThan:
-                    return Convert.ToDecimal(factValue) > Convert.ToDecimal(ruleValue);
-                case BaseComputeType.EqualsTo:
-                    return factValue.Equals(ruleValue);
-                case BaseComputeType.NotEqualsTo:
-                    return !factValue.Equals(ruleValue);
-                case BaseComputeType.Contains:
-                    return ruleValue.Contains(factValue);
-                case BaseComputeType.NotContain:
-                    return !ruleValue.Contains(factValue);
-                case BaseComputeType.LessThanOrEquals:
-                    return Convert.ToDecimal(factValue) <= Convert.ToDecimal(ruleValue);
-                case BaseComputeType.MoreThanOrEquals:
-                    return Convert.ToDecimal(factValue) >= Convert.ToDecimal(ruleValue);
-                case BaseComputeType.Any:
-                    return true;
-                case BaseComputeType.EqualsOneOfArray:
-                    return ruleValue.Split(',').Any(factValue.Equals);
-                case BaseComputeType.NotEqualsOneOfArray:
-                    return !ruleValue.Split(',').Any(factValue.Equals);
-                case BaseComputeType.RegexTrue:
-                    return Regex.IsMatch(factValue, ruleValue);
-                case BaseComputeType.RegexFalse:
-                    return !Regex.IsMatch(factValue, ruleValue);
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(computeType), computeType, $"{computeType} 找不到对应的运算方法。");
-            }
+            throw new NotImplementedException();
         }
     }
 }
